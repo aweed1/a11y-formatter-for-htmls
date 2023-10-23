d5724da61352d6ca698dda428c0e82207c86b716
Anthony Weed <aweed1@swarthmore.edu>
2023-10-23 18:07:07 -0400

    Added previews

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,26 @@
-import {useCallback, useState} from 'react'
-import TemplateComponent from 'react-mustache-template-component'
+import {useState} from 'react'
 import styled from 'styled-components'
 import {baseTemplate} from './templates/baseTemplate.js'
 import './App.css'
 import mustache from 'mustache'
-import {useDropzone} from 'react-dropzone'
 import {MetadataForm} from './forms/MetadataForm/MetadataForm.jsx'
+import beautify from 'beautify'
 
 export const App = () => {
   const [viewMode, setViewMode] = useState('preview') // Can be 'preview' or 'code'
   const [templateOutput, setTemplateOutput] = useState('')
-  const [templateBody, setTemplateBody] = useState('')
+  const [previewBody, setPreviewBody] = useState('')
 
   // Generate the HTML code for the output view by passing the supplied metadata through the mustache template
   const getCodeOutput = (templateData = {}) => {
     return mustache.render(baseTemplate, templateData)
   }
 
-  // Handle what happens when a file is dropped into the dropzone
-  const handleDrop = useCallback(files => {
-    const textDecoder = new TextDecoder('utf-8')
-
-    files.forEach(file => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('FileReader was aborted')
-      reader.onerror = () => console.log('FileReader error')
-      reader.onload = () => {
-        const buff = reader.result
-        setTemplateBody(textDecoder.decode(buff))
-      }
-      reader.readAsArrayBuffer(file)
-    })
-  }, [])
-
-  // Set up react-dropzone to accept only html files
-  const {getRootProps, getInputProps} = useDropzone({
-    onDrop: handleDrop,
-    accept: {
-      'text/html': ['.html', '.htm']
-    }
-  })
-
   const handleSubmit = formValues => {
-    setTemplateOutput(getCodeOutput({ ...formValues, content: templateBody }))
+    const code = getCodeOutput({ ...formValues })
+    setPreviewBody(formValues.content)
+    const beautified = beautify(code, { format: 'html' })
+    setTemplateOutput(beautified)
   }
 
   return (
@@ -52,17 +29,6 @@ export const App = () => {
         <div className={`border p-3 mb-3`}>
           <h2 className={`text-2xl mb-3`}>Document Metadata</h2>
           <MetadataForm onSubmit={handleSubmit} />
-        </div>
-        <div className={`border p-3`}>
-          <h2 className={`text-2xl mb-3`}>Document Upload</h2>
-
-          <div
-            {...getRootProps()}
-            className={`border-dashed border-4 border-sky-500 p-5`}
-          >
-            <input {...getInputProps()} />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-          </div>
         </div>
       </div>
       <div className={`w-1/2 border-l-2 overflow-auto`}>
@@ -84,15 +50,14 @@ export const App = () => {
         <div className="p-3">
           <div className={`border p-3`}>
             {viewMode === 'preview' && (
-              <TemplateComponent
-                template={baseTemplate}
-                data={templateOutput}
-              />
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={{ __html: previewBody }} />
             )}
             {viewMode === 'code' && (
-              <div>
-                <code>{templateOutput}</code>
-              </div>
+              <code style={{whiteSpace: 'pre'}}>
+                {templateOutput}
+              </code>
             )}
           </div>
         </div>
--- src/forms/MetadataForm/MetadataForm.jsx
+++ src/forms/MetadataForm/MetadataForm.jsx
@@ -13,18 +13,59 @@ import {
 import {SelectField} from './SelectField.jsx'
 import {TextField} from './TextField.jsx'
 import {FormProvider, useForm} from 'react-hook-form'
+import {useDropzone} from 'react-dropzone'
+import {useCallback, useState} from 'react'
 
 export const MetadataForm = ({onSubmit}) => {
+
+  // the user-provided HTML file
+  const [fileText, setFileText] = useState('')
+  // Filename for user-provided HTML file
+  const [filename, setFilename] = useState('')
+  const [filesize, setFilesize] = useState('')
+
   const methods = useForm()
 
   // run this when form is submitting
   const handleSubmit = formData => {
     const templateData = {
       id: uuidv4(),
-      ...formData
+      ...formData,
+      content: fileText
     }
+
+    console.log(templateData)
     onSubmit(templateData)
   }
+
+  // Handle what happens when a file is dropped into the dropzone
+  const handleDrop = useCallback(files => {
+    const textDecoder = new TextDecoder('utf-8')
+    files.forEach(file => {
+      const reader = new FileReader()
+      reader.onabort = () => console.log('FileReader was aborted')
+      reader.onerror = () => console.log('FileReader error')
+      reader.onload = () => {
+        const buff = reader.result
+        const txt = textDecoder.decode(buff)
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(txt, 'text/html');
+        const body = htmlDoc.querySelector('body')
+        setFileText(body.outerHTML)
+        setFilename(file.name)
+        setFilesize(file.size.toString())
+      }
+      reader.readAsArrayBuffer(file)
+    })
+  }, [])
+
+  // Set up react-dropzone to accept only html files
+  const {getRootProps, getInputProps} = useDropzone({
+    onDrop: handleDrop,
+    accept: {
+      'text/html': ['.html', '.htm']
+    }
+  })
 
   return (
     <FormProvider {...methods}>
@@ -163,12 +204,24 @@ export const MetadataForm = ({onSubmit}) => {
           options={remediationSourceOptions}
         />
 
-        <SubmitButton type={'submit'}>Submit</SubmitButton>
+        <p className={`text-2xl my-3`}>Upload your HTML file</p>
+        <div
+          {...getRootProps()}
+          className={`border-4 p-5 my-3 ${fileText.length > 0 ? 'border-solid border-green-500 bg-green-400 text-black' : 'border-dashed border-sky-500'}`}
+        >
+          <input {...getInputProps()} />
+          {fileText.length > 0 ? <>
+            <p>Uploaded {filename} - {filesize} bytes</p>
+            <p>Drag 'n' drop some files here, or click to replace {filename}</p>
+          </>: <p>Drag 'n' drop some files here, or click to select files</p>}
+        </div>
+
+        <SubmitButton type={'submit'} disabled={!methods.formState.isValid || fileText.length <= 1}>Submit</SubmitButton>
       </form>
     </FormProvider>
   )
 }
 
 const SubmitButton = styled.button.attrs({
-  className: 'rounded-sm bg-indigo-600 text-white py-1 px-3'
+  className: 'rounded-sm bg-indigo-600 text-white py-1 px-3 disabled:bg-gray-300'
 })``
